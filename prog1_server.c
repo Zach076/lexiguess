5c9de6169da5b1a62fc31bf1363a2307591af372
Zachary David Richardson <richarz@wwu.edu>
2018-10-04 13:04:09 -0700

    starting

--- prog1_server.c
+++ prog1_server.c
@@ -1,1 +1,11 @@
-// prog1_server.c
+/* CSCI 367 Lexiguess
+ *
+ * 4 OCT 2018, Zach Richardson and Mitch Kimball
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char* argv[]) {
+  
+}
