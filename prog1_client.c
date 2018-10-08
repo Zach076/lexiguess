4acf834ab4438043fe14f8422579e9b6aa99ed40
Zachary David Richardson <richarz@wwu.edu>
2018-10-08 12:29:31 -0700

    made displayboard2 function, might be garbage, might fix multiple input problem, untested but commented out

--- prog1_client.c
+++ prog1_client.c
@@ -1,7 +1,7 @@
 /* CSCI 367 Lexiguess: prog1_client.c
- *
- * 4 OCT 2018, Zach Richardson and Mitch Kimball
- */
+*
+* 4 OCT 2018, Zach Richardson and Mitch Kimball
+*/
 
 #include <stdio.h>
 #include <string.h>
@@ -29,141 +29,141 @@
 
 #define MAXWORDSIZE 254
 
-char display_board(uint8_t guesses, char* board){
-    char guess=0;
+/*
+char display_board2(uint8_t guesses, char* board, char* guess) {
+  if(guess == '\n') {
     printf("\nBoard: %s (%d guesses left) \n", board, guesses);
     printf("Enter guess: ");
     scanf("%c",&guess);
-    if(guess == '\n'){
-        scanf("%c",&guess);
-    }
-    return guess;
+  } else {
+    scanf("%c",&guess);
+  }
+  return guess;
+}
+*/
+
+char display_board(uint8_t guesses, char* board){
+  char guess=0;
+  printf("\nBoard: %s (%d guesses left) \n", board, guesses);
+  printf("Enter guess: ");
+  scanf("%c",&guess);
+  if(guess == '\n'){
+    scanf("%c",&guess);
+  }
+  return guess;
 }
 
 /*main game loop for clients*/
 void play_game(int sd) {
 
-    char guess;
-    uint8_t numguesses=0;
-    int wordlength;
-    char* sendbuffer[MAXWORDSIZE+1];
-    //create a word buffer with size numguesses+1 to store the word + a null terminator
-    char wordbuf[MAXWORDSIZE+1];
+  char* guess;
+  uint8_t numguesses=0;
+  int wordlength;
 
-    memset(sendbuffer,0,sizeof(sendbuffer));
-    memset(wordbuf,0,sizeof(wordbuf)); //this essentially adds the null  terminator to the board for us.
+  //create a word buffer with size numguesses+1 to store the word + a null terminator
+  char wordbuf[MAXWORDSIZE+1];
+  memset(wordbuf,0,sizeof(wordbuf)); //this essentially adds the null  terminator to the board for us.
 
-    //read the number of guesses
-    ssize_t n = read(sd,&numguesses,sizeof(numguesses));
+  //read the number of guesses
+  ssize_t n = read(sd,&numguesses,sizeof(numguesses));
 
-    wordlength = numguesses;
+  wordlength = numguesses;
 
-    //read the board, wait for all 6 characters of the word binary
-    n = recv(sd,wordbuf,numguesses,0);
+  //read the board, wait for all characters of the secret word
+  n = recv(sd,wordbuf,numguesses,0);
 
-    //while not done: TODO: add checking number of underscores
-    while (numguesses > 0){
-        if(numguesses == 255) {
-            //print board
-            printf("\nBoard: %s\n",wordbuf);
-            //you win
-            printf("You win\n");
-            //close socket
-            close(sd);
-            //exit
-            exit(EXIT_SUCCESS);
-        }
-
-        guess = display_board(numguesses,wordbuf);
-
-        //send the character guess to the server
-        send(sd,&guess,1,0);
-
-        memset(wordbuf,0,sizeof(wordbuf));
-        n = read(sd,&numguesses,sizeof(numguesses));
-        n = recv(sd,wordbuf,wordlength,0);
-
+  //while client still has guesses left
+  while (numguesses > 0){
+    if(numguesses == 255) {
+      //print board, you win, close socket, and exit
+      printf("\nBoard: %s\n",wordbuf);
+      printf("You win\n");
+      close(sd);
+      exit(EXIT_SUCCESS);
     }
-    //print board
-    printf("Board: %s\n",wordbuf);
-    //you lose
-    printf("You lost\n");
-    //close socket
-    close(sd);
-    //exit
-    exit(EXIT_SUCCESS);
+
+    //guess = display_board2(numguesses,wordbuf,guess);
+    guess = display_board(numguesses,wordbuf);
+    //send the character guess to the server
+    send(sd,&guess,1,0);
+    //get server response
+    memset(wordbuf,0,sizeof(wordbuf));
+    n = read(sd,&numguesses,sizeof(numguesses));
+    n = recv(sd,wordbuf,wordlength,0);
+  }
+
+  //print board, you lose, close socket, and exit
+  printf("Board: %s\n",wordbuf);
+  printf("You lost\n");
+  close(sd);
+  exit(EXIT_SUCCESS);
 }
 
+//main function, mostly connection logic
 int main( int argc, char **argv) {
-	struct hostent *ptrh; /* pointer to a host table entry */
-	struct protoent *ptrp; /* pointer to a protocol table entry */
-	struct sockaddr_in sad; /* structure to hold an IP address */
-	int sd; /* socket descriptor */
-	int port; /* protocol port number */
-	char *host; /* pointer to host name */
-	int n; /* number of characters read */
-	char buf[1000]; /* buffer for data from the server */
+  struct hostent *ptrh; /* pointer to a host table entry */
+  struct protoent *ptrp; /* pointer to a protocol table entry */
+  struct sockaddr_in sad; /* structure to hold an IP address */
+  int sd; /* socket descriptor */
+  int port; /* protocol port number */
+  char *host; /* pointer to host name */
+  int n; /* number of characters read */
+  char buf[1000]; /* buffer for data from the server */
 
-	memset((char *)&sad,0,sizeof(sad)); /* clear sockaddr structure */
-	sad.sin_family = AF_INET; /* set family to Internet */
+  memset((char *)&sad,0,sizeof(sad)); /* clear sockaddr structure */
+  sad.sin_family = AF_INET; /* set family to Internet */
 
-	if( argc != 3 ) {
-		fprintf(stderr,"Error: Wrong number of arguments\n");
-		fprintf(stderr,"usage:\n");
-		fprintf(stderr,"./prog1_client server_address server_port\n");
-		exit(EXIT_FAILURE);
-	}
+  if( argc != 3 ) {
+    fprintf(stderr,"Error: Wrong number of arguments\n");
+    fprintf(stderr,"usage:\n");
+    fprintf(stderr,"./prog1_client server_address server_port\n");
+    exit(EXIT_FAILURE);
+  }
 
-	port = atoi(argv[2]); /* convert to binary */
-	if (port > 0) /* test for legal value */
-		sad.sin_port = htons((u_short)port);
-	else {
-		fprintf(stderr,"Error: bad port number %s\n",argv[2]);
-		exit(EXIT_FAILURE);
-	}
+  port = atoi(argv[2]); /* convert to binary */
+  if (port > 0) /* test for legal value */
+  sad.sin_port = htons((u_short)port);
+  else {
+    fprintf(stderr,"Error: bad port number %s\n",argv[2]);
+    exit(EXIT_FAILURE);
+  }
 
-	host = argv[1]; /* if host argument specified */
+  host = argv[1]; /* if host argument specified */
 
-	/* Convert host name to equivalent IP address and copy to sad. */
-	ptrh = gethostbyname(host);
-	if ( ptrh == NULL ) {
-		fprintf(stderr,"Error: Invalid host: %s\n", host);
-		exit(EXIT_FAILURE);
-	}
+  /* Convert host name to equivalent IP address and copy to sad. */
+  ptrh = gethostbyname(host);
+  if ( ptrh == NULL ) {
+    fprintf(stderr,"Error: Invalid host: %s\n", host);
+    exit(EXIT_FAILURE);
+  }
 
-	memcpy(&sad.sin_addr, ptrh->h_addr, ptrh->h_length);
+  memcpy(&sad.sin_addr, ptrh->h_addr, ptrh->h_length);
 
-	/* Map TCP transport protocol name to protocol number. */
-	if ( ((long int)(ptrp = getprotobyname("tcp"))) == 0) {
-		fprintf(stderr, "Error: Cannot map \"tcp\" to protocol number");
-		exit(EXIT_FAILURE);
-	}
+  /* Map TCP transport protocol name to protocol number. */
+  if ( ((long int)(ptrp = getprotobyname("tcp"))) == 0) {
+    fprintf(stderr, "Error: Cannot map \"tcp\" to protocol number");
+    exit(EXIT_FAILURE);
+  }
 
-	/* Create a socket. */
-	sd = socket(PF_INET, SOCK_STREAM, ptrp->p_proto);
-	if (sd < 0) {
-		fprintf(stderr, "Error: Socket creation failed\n");
-		exit(EXIT_FAILURE);
-	}
+  /* Create a socket. */
+  sd = socket(PF_INET, SOCK_STREAM, ptrp->p_proto);
+  if (sd < 0) {
+    fprintf(stderr, "Error: Socket creation failed\n");
+    exit(EXIT_FAILURE);
+  }
 
-	/* TODO: Connect the socket to the specified server. You have to pass correct parameters to the connect function.*/
-	if (connect(sd, (struct sockaddr*) &sad, sizeof(sad)) < 0) {
-		fprintf(stderr,"connect failed\n");
-		exit(EXIT_FAILURE);
-	}
+  /* TODO: Connect the socket to the specified server. You have to pass correct parameters to the connect function.*/
+  if (connect(sd, (struct sockaddr*) &sad, sizeof(sad)) < 0) {
+    fprintf(stderr,"connect failed\n");
+    exit(EXIT_FAILURE);
+  }
 
 
-	/* Repeatedly read data from socket and write to user's screen. */
-    play_game(sd);
-    /*
-	n = recv(sd, buf, sizeof(buf), 0);
-	while (n > 0) {
-		write(1,buf,n);
-		n = recv(sd, buf, sizeof(buf), 0);
-	}
-    */
+  /* Game Logic function */
+  play_game(sd);
 
-	close(sd);
+  //in case real bad things happen still close the socket and exit nicely
+  close(sd);
 
-	exit(EXIT_SUCCESS);
+  exit(EXIT_SUCCESS);
 }
